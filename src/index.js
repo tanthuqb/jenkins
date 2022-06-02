623880160c17de2f65f3d109a3fb751f5b5c7454
Hoang Cong Loc <hoangconglock15@gmail.com>
2022-06-02 16:03:07 +0700

    message

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => {
+    return (
+        <div>Hi ! I'm there</div>
+    )
+
+}
+
+ReactDOM.render(
+    <App />, 
+    document.querySelector('#root')
+)
